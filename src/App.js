b5c927b1544a3bde888554e343451949ba8b1352
Matt Miles <matt_miles2@live.co.uk>
2025-03-22 20:05:35 +0000

    Add routing to NavBar links.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
+/// IMPORTS ///
 
+// React
+import { Route, Routes } from "react-router-dom";
+
+// CSS
+import styles from "./App.module.css";
+
+// Bootstrap Components
+import Container from "react-bootstrap/Container";
+
+// My Components
+import NavBar from "./components/NavBar";
+
+// APP ///
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Routes>
+          <Route path="/" element={<h1>Homepage</h1>} />
+          <Route path="/signin" element={<h1>Sign in</h1>} />
+          <Route path="/signup" element={<h1>Sign up</h1>} />
+          {/* Catch page not found */}
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
