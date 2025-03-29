919a8e9c6fabbb6eb06266f35aa52c328c694e63
Matt Miles <matt_miles2@live.co.uk>
2025-03-29 11:16:37 +0000

    Add SignInForm.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Container from "react-bootstrap/Container";
 // My Components
 import NavBar from "./components/NavBar";
 import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
 
 // APP ///
 function App() {
@@ -24,7 +25,7 @@ function App() {
       <Container className={styles.Main}>
         <Routes>
           <Route path="/" element={<h1>Homepage</h1>} />
-          <Route path="/signin" element={<h1>Sign in</h1>} />
+          <Route path="/signin" element={<SignInForm />} />
           <Route path="/signup" element={<SignUpForm />} />
           {/* Catch page not found */}
           <Route path="*" element={<h1>Page Not Found</h1>} />
