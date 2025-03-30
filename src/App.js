0318d5ba061fb0149de0ecb436bfa5281cedf0ad
Matt Miles <matt_miles2@live.co.uk>
2025-03-30 10:42:10 +0100

    Add PostPage and handle post fetch requests.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import NavBar from "./components/NavBar";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
+import PostPage from "./pages/posts/PostPage";
 
 // APP ///
 function App() {
@@ -30,6 +31,7 @@ function App() {
               <Route path="/signin" element={<SignInForm />} />
               <Route path="/signup" element={<SignUpForm />} />
               <Route path="/posts/create" element={<PostCreateForm />} />
+              <Route path="/posts/:id" element={<PostPage />} />
               {/* Catch page not found */}
               <Route path="*" element={<h3 className="text-center mt-5">You seem to be lost... click the logo to return home!</h3>} />
             </Routes>
