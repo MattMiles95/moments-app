74871c3c454cf1b5ceb731c11e434f8d7451be59
Matt Miles <matt_miles2@live.co.uk>
2025-03-30 12:11:26 +0100

    Add unlike functionality to Post component. Update Post css.

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -8,13 +8,14 @@ import { Chat } from "react-bootstrap-icons";
 import { Heart } from "react-bootstrap-icons";
 import { HeartFill } from "react-bootstrap-icons";
 
-import { postPage } from "./PostPage";
 import Avatar from "../../components/Avatar";
 
 import styles from "../../styles/Post.module.css";
 
 import { useCurrentUser } from "../../context/CurrentUserContext";
 import { Link } from "react-router-dom";
+
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Post = (props) => {
   const {
@@ -29,10 +30,44 @@ const Post = (props) => {
     content,
     image,
     updated_at,
+    postPage,
+    setPosts,
   } = props;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/likes/", { post: id });
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? { ...post, likes_count: post.likes_count + 1, like_id: data.id }
+            : post;
+        }),
+      }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleUnlike = async () => {
+    try {
+      await axiosRes.delete(`/likes/${like_id}/`);
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? { ...post, likes_count: post.likes_count - 1, like_id: null }
+            : post;
+        }),
+      }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <Card className={styles.Post}>
@@ -55,28 +90,28 @@ const Post = (props) => {
         {title && <Card.Title className="text-center">{title}</Card.Title>}
         {content && <Card.Text>{content}</Card.Text>}
         <div className={styles.PostBar}>
-            {is_owner ? (
-                <OverlayTrigger placement="top" overlay={<Tooltip>You can't like your own post!</Tooltip>}>
-                    <Heart className="mr-1" size={20} />
-                </OverlayTrigger>
-            ) : like_id ? (
-              <span onClick={() => {}}>
-                <HeartFill className={`mr-1 ${styles.FilledHeart}`} size={20} />
-              </span>
-            ) : currentUser ? (
-              <span onClick={() => {}}>
-                <Heart className={`mr-1 ${styles.HeartOutline}`} size={20} />
-              </span>
-            ) : (
-              <OverlayTrigger placement="top" overlay={<Tooltip>Log in to like posts!</Tooltip>}>
-                <Heart className="mr-1" size={20} />
-              </OverlayTrigger>
-            )}
-            {likes_count}
-            <Link to={`/posts/${id}`}>
-              <Chat className="ml-4 mr-1" size={20} />
-            </Link>
-            {comments_count}
+          {is_owner ? (
+            <OverlayTrigger placement="top" overlay={<Tooltip>You can't like your own post!</Tooltip>}>
+              <Heart className="mr-1" size={20} />
+            </OverlayTrigger>
+          ) : like_id ? (
+            <span onClick={handleUnlike}>
+              <HeartFill className={`mr-1 ${styles.Heart}`} size={20} />
+            </span>
+          ) : currentUser ? (
+            <span onClick={handleLike}>
+              <Heart className={`mr-1 ${styles.HeartOutline}`} size={20} />
+            </span>
+          ) : (
+            <OverlayTrigger placement="top" overlay={<Tooltip>Log in to like posts!</Tooltip>}>
+              <Heart className="mr-1" size={20} />
+            </OverlayTrigger>
+          )}
+          {likes_count}
+          <Link to={`/posts/${id}`}>
+            <Chat className="ml-4 mr-1" size={20} />
+          </Link>
+          {comments_count}
         </div>
       </Card.Body>
     </Card>
