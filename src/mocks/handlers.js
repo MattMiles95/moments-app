fe361936670d4045fe6c15798443f64b8079bb93
Matt Miles <matt_miles2@live.co.uk>
2025-04-02 22:01:35 +0100

    Add test handlers.

---
+++ src/mocks/handlers.js
@@ -0,0 +1,20 @@
+import { rest } from 'msw';
+
+const baseURL = 'https://drf-api-moments-app-83b302cf21ca.herokuapp.com/';
+
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(ctx.json({
+            pk: 3,
+            username: "mattmiles95",
+            email: "",
+            first_name: "",
+            last_name: "",
+            profile_id: 3,
+            profile_image: "https://res.cloudinary.com/drnx69cce/image/upload/v1/media/profile_images/Coder_Profile_Pic_lmrqol"
+            }))
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    })
+];
